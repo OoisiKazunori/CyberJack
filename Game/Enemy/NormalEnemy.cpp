bca5ddf9392808f6d92bd52002bbfc119bef58cd
LC1A_07_オオイシカズノリ <k020g1151@g.neec.ac.jp>
2022-02-12 21:39:18 +0900

    初期化の処理にコメントアウトを追加

--- Game/Enemy/NormalEnemy.cpp
+++ Game/Enemy/NormalEnemy.cpp
@@ -6,9 +6,9 @@ NormalEnemy::NormalEnemy()
 
 void NormalEnemy::Init(const XMVECTOR &POS)
 {
-	iEnemy_ModelRender->data.transform.pos = POS;
-	iEnemy_EnemyStatusData->hitBox.radius = 5.0f;
-	iOperationData.Init(1);
+	iEnemy_ModelRender->data.transform.pos = POS;	//座標の初期化
+	iEnemy_EnemyStatusData->hitBox.radius = 5.0f;	//当たり判定の大きさ変更
+	iOperationData.Init(1);	//残りロックオン数等の初期化
 }
 
 void NormalEnemy::Finalize()
