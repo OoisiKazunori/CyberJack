059cebfcbbc28920f8cc0c7f13a9a2cd4dd1437b
LC1A_07_オオイシカズノリ <k020g1151@g.neec.ac.jp>
2022-04-24 16:33:50 +0900

    RapidJsonを使用してJSONファイルの読み込みと書き込みを行い、ステージの仮置きの座標、角度、規模、フォグ密度、フォグカラーを保存する処理を追加

--- Game/Stage/FirstStage.cpp
+++ Game/Stage/FirstStage.cpp
@@ -71,41 +71,67 @@ FirstStage::FirstStage()
 		data[i][0] = 'B';
 		data[i][1] = 'o';
 		data[i][2] = 'x';
-		data[i][3] = std::to_string(i)[0];
-		data[i][4] = '\0';
+		if (i < 10)
+		{
+			data[i][3] = std::to_string(i)[0];
+			data[i][4] = '\0';
+		}
+		else
+		{
+			data[i][3] = std::to_string(i)[0];
+			data[i][4] = std::to_string(i)[1];
+			data[i][5] = '\0';
+		}
 	}
 
 
-	for (int i = 0; i < stageDebugBox.size(); ++i)
-	{
-		XMVECTOR pos = stageDebugBox[i].data.transform.pos;
-		XMVECTOR scale = stageDebugBox[i].data.transform.scale;
-		XMVECTOR rota = stageDebugBox[i].data.transform.rotation;
 
-		//Box毎のメンバ変数を追加
-		rapidjson::Value posArray(rapidjson::kArrayType);
-		rapidjson::Value scaleArray(rapidjson::kArrayType);
-		rapidjson::Value rotaArray(rapidjson::kArrayType);
-		for (int axisIndex = 0; axisIndex < 3; ++axisIndex)
+	if (false)
+	{
+
+		for (int i = 0; i < stageDebugBox.size(); ++i)
 		{
-			posArray.PushBack(rapidjson::Value(pos.m128_f32[axisIndex]), stageParamLoader.doc.GetAllocator());
-			scaleArray.PushBack(rapidjson::Value(scale.m128_f32[axisIndex]), stageParamLoader.doc.GetAllocator());
-			rotaArray.PushBack(rapidjson::Value(rota.m128_f32[axisIndex]), stageParamLoader.doc.GetAllocator());
+			XMVECTOR pos = stageDebugBox[i].data.transform.pos;
+			XMVECTOR scale = stageDebugBox[i].data.transform.scale;
+			XMVECTOR rota = stageDebugBox[i].data.transform.rotation;
+
+			//Box毎のメンバ変数を追加
+			rapidjson::Value posArray(rapidjson::kArrayType);
+			rapidjson::Value scaleArray(rapidjson::kArrayType);
+			rapidjson::Value rotaArray(rapidjson::kArrayType);
+			for (int axisIndex = 0; axisIndex < 3; ++axisIndex)
+			{
+				posArray.PushBack(rapidjson::Value(pos.m128_f32[axisIndex]), stageParamLoader.doc.GetAllocator());
+				scaleArray.PushBack(rapidjson::Value(scale.m128_f32[axisIndex]), stageParamLoader.doc.GetAllocator());
+				rotaArray.PushBack(rapidjson::Value(rota.m128_f32[axisIndex]), stageParamLoader.doc.GetAllocator());
+			}
+
+			//Boxオブジェクトにデータを追加
+			rapidjson::Value object(rapidjson::kObjectType);
+			object.AddMember("Pos", posArray, stageParamLoader.doc.GetAllocator());
+			object.AddMember("Scale", scaleArray, stageParamLoader.doc.GetAllocator());
+			object.AddMember("Rota", rotaArray, stageParamLoader.doc.GetAllocator());
+			object.AddMember("Fog", 0.0f, stageParamLoader.doc.GetAllocator());
+
+			stageParamLoader.doc.AddMember(rapidjson::GenericStringRef<char>(data[i].data()), object, stageParamLoader.doc.GetAllocator());
 		}
 
-		//Boxオブジェクトにデータを追加
-		rapidjson::Value object(rapidjson::kObjectType);
-		object.AddMember("Pos", posArray, stageParamLoader.doc.GetAllocator());
-		object.AddMember("Scale", scaleArray, stageParamLoader.doc.GetAllocator());
-		object.AddMember("Rota", rotaArray, stageParamLoader.doc.GetAllocator());
+		rapidjson::Value fogColor(rapidjson::kArrayType);
+		fogColor.PushBack(rapidjson::Value(fogData[0].fogdata.x), stageParamLoader.doc.GetAllocator());
+		fogColor.PushBack(rapidjson::Value(fogData[0].fogdata.y), stageParamLoader.doc.GetAllocator());
+		fogColor.PushBack(rapidjson::Value(fogData[0].fogdata.z), stageParamLoader.doc.GetAllocator());
+		stageParamLoader.doc.AddMember("FogColor", fogColor, stageParamLoader.doc.GetAllocator());
 
-		stageParamLoader.doc.AddMember(rapidjson::GenericStringRef<char>(data[i].data()), object, stageParamLoader.doc.GetAllocator());
 	}
+
 	stageParamLoader.ExportFile(KazFilePathName::StageParamPath + "StageParamData.json");
 }
 
 void FirstStage::Update()
 {
+	bool exportFlag = false;
+	bool importFlag = false;
+
 	ImGui::Begin("Stage");
 	for (int i = 0; i < stageDebugBox.size(); ++i)
 	{
@@ -128,14 +154,71 @@ void FirstStage::Update()
 	ImGui::InputFloat("FogColorX", &fogData[0].fogdata.x);
 	ImGui::InputFloat("FogColorY", &fogData[0].fogdata.y);
 	ImGui::InputFloat("FogColorZ", &fogData[0].fogdata.z);
+	if (ImGui::Button("Import"))
+	{
+		importFlag = true;
+	}
+	ImGui::SameLine();
+	if (ImGui::Button("Export"))
+	{
+		exportFlag = true;
+	}
 	ImGui::End();
-
 
 	for (int i = 1; i < stageDebugBox.size(); ++i)
 	{
 		fogData[i].fogdata.x = fogData[0].fogdata.x;
 		fogData[i].fogdata.y = fogData[0].fogdata.y;
 		fogData[i].fogdata.z = fogData[0].fogdata.z;
+	}
+
+	//ファイル読み込み
+	if (importFlag)
+	{
+		for (int i = 0; i < stageDebugBox.size(); ++i)
+		{
+			std::string name = "Box" + std::to_string(i);
+			for (int axisIndex = 0; axisIndex < 3; ++axisIndex)
+			{
+				stageDebugBox[i].data.transform.pos.m128_f32[axisIndex] = stageParamLoader.doc[name.c_str()]["Pos"][axisIndex].GetFloat();
+				stageDebugBox[i].data.transform.scale.m128_f32[axisIndex] = stageParamLoader.doc[name.c_str()]["Scale"][axisIndex].GetFloat();
+				stageDebugBox[i].data.transform.rotation.m128_f32[axisIndex] = stageParamLoader.doc[name.c_str()]["Rota"][axisIndex].GetFloat();
+			}
+			fogData[i].fogdata.w = stageParamLoader.doc[name.c_str()]["Fog"].GetFloat();
+		}
+
+		for (int i = 0; i < stageDebugBox.size(); ++i)
+		{
+			fogData[i].fogdata.x = stageParamLoader.doc["FogColor"][0].GetFloat();
+			fogData[i].fogdata.y = stageParamLoader.doc["FogColor"][1].GetFloat();
+			fogData[i].fogdata.z = stageParamLoader.doc["FogColor"][2].GetFloat();
+		}
+	}
+
+	//ファイル書き込み
+	if (exportFlag)
+	{
+		for (int i = 0; i < stageDebugBox.size(); ++i)
+		{
+			std::string name = "Box" + std::to_string(i);
+			for (int axisIndex = 0; axisIndex < 3; ++axisIndex)
+			{
+				stageParamLoader.doc[name.c_str()]["Pos"][axisIndex].SetFloat(stageDebugBox[i].data.transform.pos.m128_f32[axisIndex]);
+				stageParamLoader.doc[name.c_str()]["Scale"][axisIndex].SetFloat(stageDebugBox[i].data.transform.scale.m128_f32[axisIndex]);
+				stageParamLoader.doc[name.c_str()]["Rota"][axisIndex].SetFloat(stageDebugBox[i].data.transform.rotation.m128_f32[axisIndex]);
+			}
+			stageParamLoader.doc[name.c_str()]["Fog"].SetFloat(fogData[i].fogdata.w);
+		}
+
+
+		for (int i = 0; i < stageDebugBox.size(); ++i)
+		{
+			stageParamLoader.doc["FogColor"][0].SetFloat(fogData[i].fogdata.x);
+			stageParamLoader.doc["FogColor"][1].SetFloat(fogData[i].fogdata.y);
+			stageParamLoader.doc["FogColor"][2].SetFloat(fogData[i].fogdata.z);
+		}
+
+		stageParamLoader.ExportFile(KazFilePathName::StageParamPath + "StageParamData.json");
 	}
 
 	for (int i = 0; i < stageDebugBox.size(); ++i)
