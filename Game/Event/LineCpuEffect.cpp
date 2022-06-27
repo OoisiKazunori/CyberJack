938c6f1661dd1c2a2c186a919f9370de93c26f53
LC1A_07_オオイシカズノリ <k020g1151@g.neec.ac.jp>
2022-06-27 10:12:36 +0900

    CPUから伸びる線のような演出を作る為のクラスを追加

---
+++ Game/Event/LineCpuEffect.cpp
@@ -0,0 +1,78 @@
+#include "LineCpuEffect.h"
+
+LineCpuEffect::LineCpuEffect()
+{
+	for (int i = 0; i < lineRender.size(); ++i)
+	{
+		lineRender[i].data.pipelineName = PIPELINE_NAME_LINE_FLASHEFFECT;
+		constBufferHandle[i] = lineRender[i].CreateConstBuffer(sizeof(XMFLOAT4), typeid(XMFLOAT4).name(), GRAPHICS_RANGE_TYPE_CBV, GRAPHICS_PRAMTYPE_DATA);
+
+		lineEffectData[i].x = 1.0f;
+		lineEffectData[i].y = 0.0f;
+		lineEffectData[i].z = 0.0f;
+		flashTimer[i] = 0;
+	}
+	maxTimer = 60;
+}
+
+void LineCpuEffect::Init(LineEffectVec VEC, KazMath::Vec3<float> &POS)
+{
+	lineRender[0].data.startPos = POS;
+	KazMath::Vec3<float>endPos = POS;
+
+	switch (VEC)
+	{
+	case LINE_UPVEC:
+		endPos += KazMath::Vec3<float>(0.0f, 10.0f, 0.0f);
+		break;
+	case LINE_DOWNVEC:
+		endPos += KazMath::Vec3<float>(0.0f, -10.0f, 0.0f);
+		break;
+	case LINE_LEFTVEC:
+		endPos += KazMath::Vec3<float>(-10.0f, 0.0f, 0.0f);
+		break;
+	case LINE_RIGHTVEC:
+		endPos += KazMath::Vec3<float>(10.0f, 0.0f, 0.0f);
+		break;
+	default:
+		break;
+	}
+	lineRender[0].data.endPos = endPos;
+	flashFlag = false;
+}
+
+void LineCpuEffect::Update()
+{
+	if (flashFlag)
+	{
+		//フラッシュ用のデータ
+		for (int i = 0; i < lineRender.size(); ++i)
+		{
+			lineEffectData[i].w = static_cast<float>(flashTimer[i]) / static_cast<float>(maxTimer);
+			lineRender[i].TransData(&lineEffectData[i], constBufferHandle[i], typeid(XMFLOAT4).name());
+			++flashTimer[i];
+		}
+	}
+}
+
+void LineCpuEffect::Draw()
+{
+	for (int i = 0; i < lineRender.size(); ++i)
+	{
+		lineRender[i].Draw();
+	}
+}
+
+void LineCpuEffect::FlashLight()
+{
+	flashFlag = true;
+}
+
+void LineCpuEffect::InitFlash()
+{
+	flashFlag = false;
+	for (int i = 0; i < lineRender.size(); ++i)
+	{
+		flashTimer[i] = 0;
+	}
+}
---
+++ Game/Event/LineCpuEffect.h
@@ -0,0 +1,36 @@
+#pragma once
+#include"../KazLibrary/DirectXCommon/Base.h"
+#include"../KazLibrary/Math/KazMath.h"
+#include"../KazLibrary/Render/KazRender.h"
+#include<array>
+
+enum LineEffectVec
+{
+	LINE_UPVEC,
+	LINE_DOWNVEC,
+	LINE_LEFTVEC,
+	LINE_RIGHTVEC,
+};
+
+class LineCpuEffect
+{
+public:
+	LineCpuEffect();
+
+	void Init(LineEffectVec VEC, KazMath::Vec3<float> &POS);
+	void Update();
+	void Draw();
+
+	float rate;
+	void FlashLight();
+	void InitFlash();
+private:
+	static const int LINE_MAX_NUM = 1;
+	std::array<LineRender, LINE_MAX_NUM>lineRender;
+	std::array<RESOURCE_HANDLE, LINE_MAX_NUM>constBufferHandle;
+	std::array<XMFLOAT4, LINE_MAX_NUM>lineEffectData;
+	std::array<int, LINE_MAX_NUM>flashTimer;
+	int maxTimer;
+	bool flashFlag;
+};
+
