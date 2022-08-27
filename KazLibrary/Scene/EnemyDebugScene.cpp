1003321a8b1d8843df1015a83ccf0d8043e38c10
LC1A_07_オオイシカズノリ <k020g1151@g.neec.ac.jp>
2022-08-27 16:01:47 +0900

    テスト用のリソースをアップロード 使わない処理をコメントアウト

--- KazLibrary/Scene/EnemyDebugScene.cpp
+++ KazLibrary/Scene/EnemyDebugScene.cpp
@@ -43,10 +43,15 @@ EnemyDebugScene::EnemyDebugScene()
 	}
 
 
+
+
+
+	//ÉÇÉfÉãÇÃì«Ç›çûÇ›èàóù
 	battleShipObjModel.data.handle = ObjResourceMgr::Instance()->LoadModel(KazFilePathName::EnemyPath + "BattleShip/" + "BattleshipEnemy_Model.obj");	//ÉÇÉfÉãì«Ç›çûÇ›
 	battleShipFbxModel.data.handle = FbxModelResourceMgr::Instance()->LoadModel(KazFilePathName::EnemyPath + "BattleShip/" + "BattleshipEnemy_HachOpen_anim.fbx");	//ÉÇÉfÉãì«Ç›çûÇ›
 	boxModel.data.handle = FbxModelResourceMgr::Instance()->LoadModel(KazFilePathName::RelativeResourcePath + "cube.fbx");	//ÉÇÉfÉãì«Ç›çûÇ›
-	uvCheckModel.data.handle = FbxModelResourceMgr::Instance()->LoadModel(KazFilePathName::EnemyPath + "BattleShip/" + "testCube.fbx");	//ÉÇÉfÉãì«Ç›çûÇ›
+	uvCheckModel.data.handle = FbxModelResourceMgr::Instance()->LoadModel(KazFilePathName::RelativeResourcePath + "testCube.fbx");	//ÉÇÉfÉãì«Ç›çûÇ›
+
 
 	battleShipObjModel.data.transform.pos = { -350.0f,0.0f,0.0f };
 	battleShipFbxModel.data.transform.pos = { 200.0f,0.0f,0.0f };
@@ -142,121 +147,121 @@ void EnemyDebugScene::Update()
 	targetPos = KazMath::CaluTargetPosForDebug(eyePos, angle.x);
 	CameraMgr::Instance()->Camera(eyePos, targetPos, { 0.0f,1.0f,0.0f }, 0);
 
-	ImGui::Begin("EnemyList");
-	ImGui::InputInt("enemyIndex", &specifiedEnemyType);
-	initFlag = ImGui::Button("Init");
-	ImGui::SameLine();
-	deadFlag = ImGui::Button("Dead");
-	ImGui::SameLine();
-	attackFlag = ImGui::Button("Attack");
-	ImGui::DragFloat("ResponePosX", &responePos.x);
-	ImGui::DragFloat("ResponePosY", &responePos.y);
-	ImGui::DragFloat("ResponePosZ", &responePos.z);
-	ImGui::End();
+	//ImGui::Begin("EnemyList");
+	//ImGui::InputInt("enemyIndex", &specifiedEnemyType);
+	//initFlag = ImGui::Button("Init");
+	//ImGui::SameLine();
+	//deadFlag = ImGui::Button("Dead");
+	//ImGui::SameLine();
+	//attackFlag = ImGui::Button("Attack");
+	//ImGui::DragFloat("ResponePosX", &responePos.x);
+	//ImGui::DragFloat("ResponePosY", &responePos.y);
+	//ImGui::DragFloat("ResponePosZ", &responePos.z);
+	//ImGui::End();
 
-	//ÉTÉCÉYè„å¿--------------------------------------
-	if (specifiedEnemyType <= -1)
-	{
-		specifiedEnemyType = 0;
-	}
-	else if (enemies.size() <= specifiedEnemyType)
-	{
-		specifiedEnemyType = static_cast<int>(enemies.size() - 1);
-	}
-	//ÉTÉCÉYè„å¿--------------------------------------
-
-
-	//èâä˙âª--------------------------------------
-	if (specifiedEnemyType != oldEnemyType || initFlag)
-	{
-		if (oldEnemyType <= -1)
-		{
-			oldEnemyType = 0;
-		}
-
-		//ìGÇ∆éqìGÇÃéÄñS--------------------------------------
-		if (oldEnemyType != -1)
-		{
-			enemies[oldEnemyType]->Dead();
-
-			for (int i = 0; i < misiles[oldEnemyType].size(); ++i)
-			{
-				if (misiles[oldEnemyType][i] != nullptr)
-				{
-					misiles[oldEnemyType][i]->Dead();
-				}
-			}
-		}
-		kidEnemyHandles[oldEnemyType] = 0;
-		//ìGÇ∆éqìGÇÃéÄñS--------------------------------------
-
-		//èâä˙âª--------------------------------------
-		EnemyGenerateData lData;
-		lData.initPos = responePos;
-		lData.battleShipData.isShotFlag = true;
-		enemies[specifiedEnemyType]->Init(lData, true);
-		//èâä˙âª--------------------------------------
-
-		//ä»à’ÉcÅ[ÉãÇÃèâä˙âª--------------------------------------
-		gameFlame = 0;
-		initFlag = false;
-		oldEnemyType = specifiedEnemyType;
-		//ä»à’ÉcÅ[ÉãÇÃèâä˙âª--------------------------------------
-	}
-	//èâä˙âª--------------------------------------
-
-	//éÄñS--------------------------------------
-	if (deadFlag)
-	{
-		enemies[specifiedEnemyType]->Dead();
-		deadFlag = false;
-	}
-	//éÄñS--------------------------------------
+	////ÉTÉCÉYè„å¿--------------------------------------
+	//if (specifiedEnemyType <= -1)
+	//{
+	//	specifiedEnemyType = 0;
+	//}
+	//else if (enemies.size() <= specifiedEnemyType)
+	//{
+	//	specifiedEnemyType = static_cast<int>(enemies.size() - 1);
+	//}
+	////ÉTÉCÉYè„å¿--------------------------------------
 
 
-	//çUåÇ--------------------------------------
-	if (attackFlag)
-	{
-		enemies[specifiedEnemyType]->DebugShot();
-		kidEnemyHandles[specifiedEnemyType] = 0;
-		attackFlag = false;
-	}
-	//çUåÇ--------------------------------------
+	////èâä˙âª--------------------------------------
+	//if (specifiedEnemyType != oldEnemyType || initFlag)
+	//{
+	//	if (oldEnemyType <= -1)
+	//	{
+	//		oldEnemyType = 0;
+	//	}
 
-	EnemyData *lEnemyData = enemies[specifiedEnemyType]->GetData().get();
-	if (enemies[specifiedEnemyType]->GetData()->genarateData.enemyType != -1)
-	{
-		//ê∂ê¨Ç∑ÇÈìGÇÃéÌóﬁ
-		misiles[specifiedEnemyType][kidEnemyHandles[specifiedEnemyType]]->Init(lEnemyData->genarateData.enemyGenerateData);
+	//	//ìGÇ∆éqìGÇÃéÄñS--------------------------------------
+	//	if (oldEnemyType != -1)
+	//	{
+	//		enemies[oldEnemyType]->Dead();
 
-		++kidEnemyHandles[specifiedEnemyType];
-		//í«â¡ÇµÇΩÇÁèIóπèàóùÇì¸ÇÍÇÈ
-		lEnemyData->genarateData.Finalize();
-	}
+	//		for (int i = 0; i < misiles[oldEnemyType].size(); ++i)
+	//		{
+	//			if (misiles[oldEnemyType][i] != nullptr)
+	//			{
+	//				misiles[oldEnemyType][i]->Dead();
+	//			}
+	//		}
+	//	}
+	//	kidEnemyHandles[oldEnemyType] = 0;
+	//	//ìGÇ∆éqìGÇÃéÄñS--------------------------------------
 
-	for (int i = 0; i < misiles[specifiedEnemyType].size(); ++i)
-	{
-		if (misiles[specifiedEnemyType][i] != nullptr && misiles[specifiedEnemyType][i]->GetData()->timer <= 0)
-		{
-			misiles[specifiedEnemyType][i]->Hit();
-		}
-	}
+	//	//èâä˙âª--------------------------------------
+	//	EnemyGenerateData lData;
+	//	lData.initPos = responePos;
+	//	lData.battleShipData.isShotFlag = true;
+	//	enemies[specifiedEnemyType]->Init(lData, true);
+	//	//èâä˙âª--------------------------------------
+
+	//	//ä»à’ÉcÅ[ÉãÇÃèâä˙âª--------------------------------------
+	//	gameFlame = 0;
+	//	initFlag = false;
+	//	oldEnemyType = specifiedEnemyType;
+	//	//ä»à’ÉcÅ[ÉãÇÃèâä˙âª--------------------------------------
+	//}
+	////èâä˙âª--------------------------------------
+
+	////éÄñS--------------------------------------
+	//if (deadFlag)
+	//{
+	//	enemies[specifiedEnemyType]->Dead();
+	//	deadFlag = false;
+	//}
+	////éÄñS--------------------------------------
 
 
-	enemies[specifiedEnemyType]->Update();
+	////çUåÇ--------------------------------------
+	//if (attackFlag)
+	//{
+	//	enemies[specifiedEnemyType]->DebugShot();
+	//	kidEnemyHandles[specifiedEnemyType] = 0;
+	//	attackFlag = false;
+	//}
+	////çUåÇ--------------------------------------
 
-	hitBox.data.transform.pos = *enemies[specifiedEnemyType]->GetData()->hitBox.center;
-	float lScale = enemies[specifiedEnemyType]->GetData()->hitBox.radius;
-	hitBox.data.transform.scale = { lScale ,lScale,lScale };
-	hitBox.data.pipelineName = PIPELINE_NAME_COLOR_WIREFLAME;
+	//EnemyData *lEnemyData = enemies[specifiedEnemyType]->GetData().get();
+	//if (enemies[specifiedEnemyType]->GetData()->genarateData.enemyType != -1)
+	//{
+	//	//ê∂ê¨Ç∑ÇÈìGÇÃéÌóﬁ
+	//	misiles[specifiedEnemyType][kidEnemyHandles[specifiedEnemyType]]->Init(lEnemyData->genarateData.enemyGenerateData);
 
-	for (int i = 0; i < misiles[specifiedEnemyType].size(); ++i)
-	{
-		if (misiles[specifiedEnemyType][i] != nullptr && misiles[specifiedEnemyType][i]->GetData()->oprationObjData->initFlag)
-		{
-			misiles[specifiedEnemyType][i]->Update();
-		}
-	}
+	//	++kidEnemyHandles[specifiedEnemyType];
+	//	//í«â¡ÇµÇΩÇÁèIóπèàóùÇì¸ÇÍÇÈ
+	//	lEnemyData->genarateData.Finalize();
+	//}
+
+	//for (int i = 0; i < misiles[specifiedEnemyType].size(); ++i)
+	//{
+	//	if (misiles[specifiedEnemyType][i] != nullptr && misiles[specifiedEnemyType][i]->GetData()->timer <= 0)
+	//	{
+	//		misiles[specifiedEnemyType][i]->Hit();
+	//	}
+	//}
+
+
+	//enemies[specifiedEnemyType]->Update();
+
+	//hitBox.data.transform.pos = *enemies[specifiedEnemyType]->GetData()->hitBox.center;
+	//float lScale = enemies[specifiedEnemyType]->GetData()->hitBox.radius;
+	//hitBox.data.transform.scale = { lScale ,lScale,lScale };
+	//hitBox.data.pipelineName = PIPELINE_NAME_COLOR_WIREFLAME;
+
+	//for (int i = 0; i < misiles[specifiedEnemyType].size(); ++i)
+	//{
+	//	if (misiles[specifiedEnemyType][i] != nullptr && misiles[specifiedEnemyType][i]->GetData()->oprationObjData->initFlag)
+	//	{
+	//		misiles[specifiedEnemyType][i]->Update();
+	//	}
+	//}
 
 }
 
@@ -269,15 +274,15 @@ void EnemyDebugScene::Draw()
 
 	bg.Draw();
 
-	player.Draw();
+	//player.Draw();
 
-	for (int i = 0; i < misiles[specifiedEnemyType].size(); ++i)
-	{
-		if (misiles[specifiedEnemyType][i] != nullptr)
-		{
-			//misiles[specifiedEnemyType][i]->Draw();
-		}
-	}
+	//for (int i = 0; i < misiles[specifiedEnemyType].size(); ++i)
+	//{
+	//	if (misiles[specifiedEnemyType][i] != nullptr)
+	//	{
+	//		//misiles[specifiedEnemyType][i]->Draw();
+	//	}
+	//}
 	//enemies[specifiedEnemyType]->Draw();
 	//hitBox.Draw();
 	//kidFbxModel.Draw();
