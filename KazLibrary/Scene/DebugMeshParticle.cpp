eca6c59c686beb882885a6eab369898ec7b1c67b
LC1A_07_オオイシカズノリ <k020g1151@g.neec.ac.jp>
2022-11-06 16:19:08 +0900

    当たり判定の処理を除いたパーティクルの初期化処理を追加

--- KazLibrary/Scene/DebugMeshParticle.cpp
+++ KazLibrary/Scene/DebugMeshParticle.cpp
@@ -57,22 +57,6 @@ void DebugMeshParticleScene::Update()
 	ImGui::SliderInt("ParticleNum", &drawParticleNum, 0, PARTICLE_MAX_NUM);
 	ImGui::End();
 
-	//パーティクルの計算処理
-	if (initFlag)
-	{
-		//以下の項目をパーティクル毎に計算し、配置する
-		for (int i = 0; i < particle.size(); ++i)
-		{
-			//1.レイに沿った座標を乱数で決定する
-			//2.指定した座標から垂直にレイを飛ばす。レイは二方向に飛ばし、別のレイと当たった方向に伸ばす
-			//3.1で決めた座標を始点、別のレイが当たった座標を終点とし、その長さを求める
-			//4.先程決めた長さの内、エッジ周辺にパーティクルを配置するか、長さの範囲で座標を決めるか乱数で決める
-			//5-1.エッジ周辺にパーティクルを配置する場合は、長さを3割った値から乱数でどの場所に配置するか決める
-			//5-2.長さの範囲で座標を決める場合は長さの値でどの場所に配置するか決める
-		}
-		initFlag = false;
-	}
-
 
 	triangelLine[0].data.startPos = triangelPosArray[0];
 	triangelLine[0].data.endPos = triangelPosArray[1];
@@ -81,6 +65,53 @@ void DebugMeshParticleScene::Update()
 	triangelLine[2].data.startPos = triangelPosArray[2];
 	triangelLine[2].data.endPos = triangelPosArray[0];
 
+
+	//パーティクルの計算処理
+	if (initFlag)
+	{
+		int particleNum = 0;
+		for (int rayIndex = 0; rayIndex < triangelLine.size(); ++rayIndex)
+		{
+			//一つのレイ当たりで使用できるパーティクルを決定する
+			const int L_MAX_PARTICLE = static_cast<int>(particle.size()) / static_cast<int>(triangelPosArray.size());
+
+			KazMath::Vec3<float>lDistance = triangelLine[rayIndex].data.endPos - triangelLine[rayIndex].data.startPos;
+
+			//以下の項目を計算し、配置する
+			for (int i = 0; i < L_MAX_PARTICLE; ++i)
+			{
+				//1.レイに沿った座標を乱数で決定する
+				float lRate = KazMath::Rand(1.0f, 0.0f);
+				KazMath::Vec3<float>lStartPos = triangelLine[rayIndex].data.startPos + lDistance * lRate;
+
+				//2.指定した座標から垂直にレイを飛ばす。レイは二方向に飛ばし、別のレイと当たった方向に伸ばす
+
+
+				//3.1で決めた座標を始点、別のレイが当たった座標を終点とし、その長さを求める
+				KazMath::Vec3<float>lResultDistance;
+
+
+
+				//4.先程決めた長さの内、エッジ周辺にパーティクルを配置するか、長さの範囲で座標を決めるか乱数で決める
+
+				//数が多いほどエッジ周辺に偏らせる
+				const int L_RANDOM_NUMBER_BIAS = 80;
+
+				//5-1.エッジ周辺にパーティクルを配置する場合は、長さを3割った値から乱数でどの場所に配置するか決める
+				if (L_RANDOM_NUMBER_BIAS <= KazMath::Rand(100, 0))
+				{
+					particle[particleNum].data.transform.pos = lStartPos + (lResultDistance / 3.0) * KazMath::Rand(1.0f, 0.0f);
+				}
+				//5-2.長さの範囲で座標を決める場合は長さの値でどの場所に配置するか決める
+				else
+				{
+					particle[particleNum].data.transform.pos = lStartPos + lResultDistance * KazMath::Rand(1.0f, 0.0f);
+				}
+				++particleNum;
+			}
+		}
+		initFlag = false;
+	}
 
 }
 
