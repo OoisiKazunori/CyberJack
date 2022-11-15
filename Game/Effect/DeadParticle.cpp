2fa14c8272400b7693be1346e71d7c8e0199f63e
LC1A_07_オオイシカズノリ <k020g1151@g.neec.ac.jp>
2022-11-15 09:42:16 +0900

    Deadparticleのスケルトンコード作成

---
+++ Game/Effect/DeadParticle.cpp
@@ -0,0 +1,23 @@
+#include"DeadParticle.h"
+
+DeadParticle::DeadParticle()
+{
+
+}
+
+void DeadParticle::Init(void* BUFFER_PTR)
+{
+	//初期化用処理
+	void *p = BUFFER_PTR;
+	p = nullptr;
+}
+
+void DeadParticle::Update()
+{
+	//更新用処理
+}
+
+void DeadParticle::Draw()
+{
+	//描画用処理
+}
---
+++ Game/Effect/DeadParticle.h
@@ -0,0 +1,11 @@
+
+class DeadParticle
+{
+public:
+	DeadParticle();
+	void Init(void *BUFFER_PTR);
+	void Update();
+	void Draw();
+
+private:
+};
