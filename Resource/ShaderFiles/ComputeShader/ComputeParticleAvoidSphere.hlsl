c39b1a51e633b00652243c3cfc9e13ce13ad780e
LC1A_07_オオイシカズノリ <k020g1151@g.neec.ac.jp>
2023-01-17 01:49:52 +0900

    対象判定描画

--- Resource/ShaderFiles/ComputeShader/ComputeParticleAvoidSphere.hlsl
+++ Resource/ShaderFiles/ComputeShader/ComputeParticleAvoidSphere.hlsl
@@ -17,26 +17,26 @@ RWStructuredBuffer<HitIDData> hitIndexData : register(u1);
 AppendStructuredBuffer<GPUParticleInput> inputGPUParticleData : register(u2);
 
 //周りの当たり判定ともとる
-//bool CheckLinkHitBox(uint3 BASE_HIT_INDEX,uint3 CHECK_HIT_INDEX)
-//{
-//    bool linkHitFlagArray[7];
-//    linkHitFlagArray[0] = CHECK_HIT_INDEX == BASE_HIT_INDEX;                    //真ん中
-//    linkHitFlagArray[1] = CHECK_HIT_INDEX == (BASE_HIT_INDEX + float3(0,1,0));  //上
-//    linkHitFlagArray[2] = CHECK_HIT_INDEX == (BASE_HIT_INDEX + float3(0,-1,0)); //下
-//    linkHitFlagArray[3] = CHECK_HIT_INDEX == (BASE_HIT_INDEX + float3(-1,0,0)); //左
-//    linkHitFlagArray[4] = CHECK_HIT_INDEX == (BASE_HIT_INDEX + float3(1,0,0));  //右
-//    linkHitFlagArray[5] = CHECK_HIT_INDEX == (BASE_HIT_INDEX + float3(0,0,1));  //前
-//    linkHitFlagArray[6] = CHECK_HIT_INDEX == (BASE_HIT_INDEX + float3(0,0,-1)); //後
-//
-//    for(int i = 0;i < 7;++i)
-//    {
-//        if(linkHitFlagArray[i])
-//        {
-//            return true;
-//        }
-//    }
-//    return false;
-//}
+bool CheckLinkHitBox(uint3 BASE_HIT_INDEX,uint3 CHECK_HIT_INDEX)
+{
+    bool linkHitFlagArray[7];
+    linkHitFlagArray[0] = CHECK_HIT_INDEX == BASE_HIT_INDEX;                    //真ん中
+    linkHitFlagArray[1] = CHECK_HIT_INDEX == (BASE_HIT_INDEX + float3(0,1,0));  //上
+    linkHitFlagArray[2] = CHECK_HIT_INDEX == (BASE_HIT_INDEX + float3(0,-1,0)); //下
+    linkHitFlagArray[3] = CHECK_HIT_INDEX == (BASE_HIT_INDEX + float3(-1,0,0)); //左
+    linkHitFlagArray[4] = CHECK_HIT_INDEX == (BASE_HIT_INDEX + float3(1,0,0));  //右
+    linkHitFlagArray[5] = CHECK_HIT_INDEX == (BASE_HIT_INDEX + float3(0,0,1));  //前
+    linkHitFlagArray[6] = CHECK_HIT_INDEX == (BASE_HIT_INDEX + float3(0,0,-1)); //後
+
+    for(int i = 0;i < 7;++i)
+    {
+        if(linkHitFlagArray[i])
+        {
+            return true;
+        }
+    }
+    return false;
+}
 
 //メッシュパーティクルの球判定を取ったインデックスを参考にパーティクルの挙動を決める
 [numthreads(1024, 1, 1)]
@@ -48,7 +48,7 @@ void CSmain(uint3 groupId : SV_GroupID, uint groupIndex : SV_GroupIndex,uint3 gr
     //for(int i = 0;i < hitIndexCounter[0]; ++i)
     //{
     //    //当たり判定+リンク付け込みの処理を取る
-    //    if(CheckLinkHitBox(hitIndexData[i],hitBoxData[index].id))
+    //    if(CheckLinkHitBox(hitIndexData[i].id,hitBoxData[index].id))
     //    {
     //        float3 posHitBoxVec = pos;
     //        float3 posParticleVec = hitBoxData[index].pos - pos;
@@ -65,7 +65,6 @@ void CSmain(uint3 groupId : SV_GroupID, uint groupIndex : SV_GroupIndex,uint3 gr
     //
     //        //パーティクルの基本座標とラープ用の座標が必要
     //        float3 larpPos = float3(0,0,0) + vel;
-    //
     //    }
     //}
 
